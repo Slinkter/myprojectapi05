224e9fb3b3e2bcd8d0cd86cb63b2733204288c0e
Slinkter <luis.j.cueva@gmail.com>
2024-04-17 17:46:49 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-
-/*  */
-import { Avatar } from "@material-tailwind/react";
-import Navbar from "./components/navbar";
-import Home from "./pages/home";
-import Favorites from "./pages/favorites";
-import Details from "./pages/details";
 
 const App = () => {
-  return (
-    <div className="">
-      <div className="min-h-screen p-6 bg-white">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/recipes-item/:id" element={<Details />} />
-        </Routes>
-      </div>
-    </div>
-  );
+  return <div>hola</div>;
 };
 
 export default App;
