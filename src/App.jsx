7e16664b1edcfe7be68bea84b300a173b3143709
Slinkter <luis.j.cueva@gmail.com>
2024-08-01 21:03:26 -0500

    revision

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-    return <div>hola</div>;
+    return (
+        <div className="min-h-dvh flex justify-center  items-center ">
+            <h1>myprojectapi05</h1>
+        </div>
+    );
 };
 
 export default App;
