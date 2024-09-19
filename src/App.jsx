5dbafa35d136c8165777ff44531b380d33f722f7
Slinkter <luis.j.cueva@gmail.com>
2024-09-19 09:40:16 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Spinner } from "@material-tailwind/react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Button, Spinner, Typography } from "@material-tailwind/react";
 import { Input } from "@material-tailwind/react";
 import CardUser from "./components/CardUser";
 
@@ -7,23 +7,72 @@ const App = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     //
+    const [userName, setUserName] = useState("slinkter");
+    const [searchText, setSearchText] = useState("");
+    const [userData, setUserData] = useState(null);
+
+    const getData = useCallback(async () => {
+        try {
+            setLoading(true);
+            const url = `https://api.github.com/users/${userName}`;
+            const res = await fetch(url);
+            const data = await res.json();
+            if (res.status === 403) {
+                throw "API rate limit exceeded";
+            }
+            if (res.status !== 200) {
+                throw "error  no es 200 ";
+            }
+            setUserData(data);
+        } catch (error) {
+            setError(error.message);
+            console.log(error);
+        } finally {
+            setLoading(false);
+            searchText("");
+        }
+    }, [userName]);
+
+    const btnSearch = useCallback(() => {
+        if (searchText.length <= 0 && searchText.trim() === "") {
+            alert("nombre vacio");
+            return;
+        }
+        setUserName(searchText);
+    }, [searchText]);
 
     useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 1000);
+        getData();
+    }, [userName, getData]);
 
-        return () => {};
-    }, []);
+    const userNotFound = useMemo(
+        () => !userData && !loading && !error,
+        [userData, loading, error]
+    );
 
     return (
         <div className="min-h-dvh flex flex-col justify-center  items-center ">
             {loading && <Spinner color="red" className="h-12 w-12" />}
-
-            {!loading && (
+            {error && <ErroShow error={error} />}
+            {userNotFound && <span>No se encontró el usuario</span>}
+            {!loading && !error && (
                 <div>
-                    <Input className="w-96" label="Username" color="blue" />
-                    <CardUser />
+                    <div className="relative flex w-full max-w-[24rem]">
+                        <Input
+                            type="email"
+                            label="username"
+                            value={searchText}
+                            onChange={(e) => setSearchText(e.target.value)}
+                        />
+                        <Button
+                            className="!absolute right-1 top-1 rounded"
+                            size="sm"
+                            onClick={btnSearch}
+                        >
+                            Search
+                        </Button>
+                    </div>
+                    <CardUser data={userData} />
                 </div>
             )}
         </div>
@@ -31,3 +80,20 @@ const App = () => {
 };
 
 export default App;
+
+const ErroShow = (error) => {
+    <div>
+        <Typography variant="h2" className="mb-2">
+            Error : {error}
+            {error === "API rate limit exceeded" && (
+                <div>
+                    <p>
+                        Has excedido el límite de llamadas a la API. Intenta de
+                        nuevo más tarde o autentica tus solicitudes para obtener
+                        un límite mayor.
+                    </p>
+                </div>
+            )}
+        </Typography>
+    </div>;
+};
--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -4,52 +4,59 @@ import {
     CardFooter,
     Typography,
     Button,
+    CardHeader,
 } from "@material-tailwind/react";
 import React from "react";
 import { Avatar } from "@material-tailwind/react";
 
-const CardUser = ({}) => {
+const CardUser = ({ data }) => {
+    const createdDateAt = new Date(data?.created_at);
+    const dayDate =
+        createdDateAt.getDate() +
+        " " +
+        createdDateAt.toLocaleDateString("en-us", { month: "short" }) +
+        " " +
+        createdDateAt.getFullYear();
     return (
-        <Card className="mt-6 w-96">
-            <CardBody>
-                <div className="flex gap-4">
+        <Card
+            className="mt-6 w-96 shadow-xl border-2 border-gray-400"
+            color=""
+            variant="gradient"
+        >
+            <CardHeader floated={false} shadow={false} color="transparent">
+                <div className="flex gap-2 justify-center items-center mt-2">
                     <Avatar
-                        src="https://docs.material-tailwind.com/img/face-2.jpg"
+                        src={
+                            data?.avatar_url ||
+                            "https://docs.material-tailwind.com/img/face-2.jpg"
+                        }
                         alt="avatar"
-                    />
-                    <Avatar
-                        src="https://docs.material-tailwind.com/img/face-2.jpg"
-                        alt="avatar"
-                        variant="rounded"
-                    />
-                    <Avatar
-                        src="https://docs.material-tailwind.com/img/face-2.jpg"
-                        alt="avatar"
-                        variant="square"
+                        size="xxl"
                     />
                 </div>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                    UI/UX Review Check
-                </Typography>
-                <Typography>
-                    Because it&apos;s about motivating the doers. Because
-                    I&apos;m here to follow my dreams and inspire others.
-                </Typography>
-
-                <div className="">
+            </CardHeader>
+            <CardBody>
+                <div className="flex flex-row gap-4 justify-center items-center text-center">
                     <div>
                         <p>Public Repos</p>
-                        <p>{123}</p>
+                        <p>{data?.public_repos || "0"}</p>
                     </div>
                     <div>
                         <p>Followers</p>
-                        <p>{32}</p>
+                        <p>{data?.followers || "0"}</p>
                     </div>
                     <div>
                         <p> Following </p>
-                        <p>{321} </p>
+                        <p>{data?.following || "0"}</p>
                     </div>
                 </div>
+
+                <Typography variant="h5" className="mb-2 text-center">
+                    {data?.name || data?.login || "username"}
+                </Typography>
+                <Typography className="mb-2 text-center">
+                    User joined on {dayDate}
+                </Typography>
             </CardBody>
             <CardFooter className="pt-0"></CardFooter>
         </Card>
